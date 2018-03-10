d491ff20cf060b92c733a043b5c1d08d44aae4ab
Daniel Conner <dannyconner@hotmail.co.uk>
2018-03-10 12:07:38 +0000

    new'd up my shopTest class and test can get name and total sales

--- src/main/java/Shop.java
+++ src/main/java/Shop.java
@@ -1,2 +1,22 @@
+import java.util.ArrayList;
+
 public class Shop {
+
+    private String name;
+    private ArrayList<Sellable> stock;
+    private double totalSales;
+
+    public Shop(String name, ArrayList<Sellable> stock, double totalSales) {
+        this.name = name;
+        this.stock = stock;
+        this.totalSales = totalSales;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getTotalSales() {
+        return totalSales;
+    }
 }
--- src/test/java/ShopTest.java
+++ src/test/java/ShopTest.java
@@ -1,2 +1,40 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import static org.junit.Assert.assertEquals;
+
 public class ShopTest {
+
+    Shop shop;
+    Guitar guitar;
+    Trumpet trumpet;
+    Harmonica harmonica;
+    Mouthpeice mouthpeice;
+
+    @Before
+    public void setUp(){
+        guitar = new Guitar("Guitar", "Epiphone", "Les Paul", "Sunburst", InstrumentFamilyType.PLUCKED_STRING, 89.99, 259.99, 10, 6);
+        trumpet = new Trumpet("Trumpet", "Elkhart", "100Tr", "Goldust", InstrumentFamilyType.WIND, 79.99, 137.78, 8, 5);
+        harmonica = new Harmonica("Harmonica", "Suzuki", "Hammond", "Steel Grey", InstrumentFamilyType.WIND, 9.00, 28.00, 5, "Detonic");
+        mouthpeice = new Mouthpeice("Rico Trumpet Mouthpiece", 25.99, 58.99, 5, "Silver");
+        ArrayList<Sellable> stock = new ArrayList<>();
+        stock.add(guitar);
+        stock.add(trumpet);
+        stock.add(harmonica);
+        stock.add(mouthpeice);
+        shop = new Shop("Auld Dannys Harmonica Haberdashery", stock, 0.00);
+    }
+
+    @Test
+    public void canGetName() {
+        assertEquals("Auld Dannys Harmonica Haberdashery", shop.getName());
+    }
+
+    @Test
+    public void canGetTotalSales() {
+        assertEquals(0.00, shop.getTotalSales(), 0.01);
+    }
 }
